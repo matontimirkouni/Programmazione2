1a057fd38835609c9f0a765d84e94fd322c671d8
matontimirko <g.matonti@studenti.unipi.it>
2018-12-13 00:51:29 +0100

    env

--- Ocaml/int.ml
+++ Ocaml/int.ml
@@ -3,6 +3,7 @@
 (* Operazioni *)
 type op = Plus | Minus | Times | Div ;;
 
+(* Identificatore *)
 type ide = string;;
 
 (* Espressioni *)
@@ -24,6 +25,16 @@ type exp =
 exception Unbound;;
 
 
+(* the empty environment *)
+let emptyenv = [];;
+
+
+let rec lookup env x =
+	match env with
+	| [] -> failwith (" not found")
+	| (y,v) :: r -> if x = y then v else lookup r x;;
+
+
 
 (* interprete op *)
 let evalop (v1:exp) (ope:op) (v2:exp) : exp =
@@ -40,25 +51,30 @@ let asint = function Int x -> x | _ -> failwith "not integer";;
 let asstring = function Ide a -> a | _ -> failwith "not string";;
 
 (* interprete *)
-let rec eval  e =
+let rec eval  e env =
 	match e with
 		| Int i -> Int i
-		| Ide i -> Ide i (* Attenzione non corretto va fatto il bind *)
+		| Ide i -> lookup env i 
+		| Let (i,dich,body) -> 
+				let ival = eval dich env in (*valuta le dichiarazioni*)
+				let env1 = (i,ival) :: env in (* aggiunge le dichiarazini nell' ambiente locale*)
+				eval body env1
 		| True -> True
 		| False -> False
-		| Not(exp0) -> (match eval exp0 with
+		| Not(exp0) -> (match eval exp0 env with
 							| True -> False
 							| False -> True
 							| _ -> raise Unbound)
-		| And(exp1,exp2) -> (match (eval exp1,eval exp2) with
+		| And(exp1,exp2) -> (match (eval exp1 env,eval exp2 env) with
 								| (True,True) -> True
 								| (_,_) -> False)
-		| Op_e(exp0,op0,exp1) -> let ev1 = eval exp0 in
-								let ev2 = eval exp1 in
+		| Op_e(exp0,op0,exp1) -> let ev1 = eval exp0 env in
+								 let ev2 = eval exp1 env in
 								evalop ev1 op0 ev2
-		| Add(exp0,exp1) -> Int(asint(eval exp0) +  asint(eval exp1))
-		| Minus(exp0,exp1) -> Int(asint(eval exp0) -  asint(eval exp1))
-		| Times(exp0,exp1) -> Int(asint(eval exp0) *  asint(eval exp1));; 
+		| Add(exp0,exp1) -> Int(asint(eval exp0 env ) +  asint(eval exp1 env))
+		| Minus(exp0,exp1) -> Int(asint(eval exp0 env) -  asint(eval exp1 env))
+		| Times(exp0,exp1) -> Int(asint(eval exp0 env) *  asint(eval exp1 env))
+		| _ -> raise Unbound;;
 
 
 
@@ -70,6 +86,18 @@ let p4= Op_e(Int(5),Div,Int(10));;
 let p5 = Add(Int(5),Int(10));;
 let p6 = Times(Int(5),Int(10));;
 let p7 = Times(Ide("lol"),Ide("lols"));;
+
+(*let x = 5+5 in x *)
+let ll = Let("x",Add(Int(5),Int(5)),Ide("x"))
+
+
+eval(Let("x", Sum(Eint 1, Eint 0),
+ Let("y", Ifthenelse(Eq(Den "x", Eint 0),
+ Diff(Den "x", Eint 1),
+ Sum(Den "x", Eint 1)),
+ Let("z", Sum(Den "x", Den "y"), Den "z"))),
+ (emptyenv Unbound));;
+
 eval p3;;
 
 									
